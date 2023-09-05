b7db4ac24ffb0247ee803a800087333a758d2497
Werton <wertonf4bian@gmail.com>
2023-09-05 17:55:27 -0300

    :tada: initial commit

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,10 +1,17 @@
 function toggleMode() {
   const html = document.documentElement
-  /*
-  if (html.classList.contains("light")) {
-    html.classList.remove("light")
-  } else {
-    html.classList.add("light")
-  } */
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/assets/light.png")
+  } else {
+    img.setAttribute("src", "./assets/assets/whats.png")
+  }
+
+  // pegar a tag img
+
+  //se tiver light mode, adicionar a imagem light
+  // substituir a imagem
 }
